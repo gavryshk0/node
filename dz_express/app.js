694dd1beecf39938dfccaccabbd597719c6ca63b
gavryshk0 <gavrishkohf@gmail.com>
2022-04-29 23:10:10 +0300

    hw

--- dz_express/app.js
+++ dz_express/app.js
@@ -1,41 +1,42 @@
 const express = require('express');
 const app = express();
-const { engine } = require('express-handlebars');
+const {engine} = require('express-handlebars');
 const dbUsers = require('./db/users');
 const dbCars = require('./db/cars');
 
-app.engine('.hbs', engine({ defaultLayout: false }));
+app.engine('.hbs', engine({defaultLayout: false}));
 app.set('view engine', '.hbs');
 app.set('views', './static');
 
-app.listen(5000,() => {
+app.listen(5000, () => {
     console.log('Done...');
 })
 
-app.get('/',(req, res) => {
+app.get('/', (req, res) => {
     res.render('welcome');
 })
 
-app.get('/users',(req, res) => {
-    res.render('users',{dbUsers});
+app.get('/users', (req, res) => {
+    res.render('users', {dbUsers});
 })
 
-app.get('/users/:userID', (req, res) =>{
+app.get('/users/:userID', (req, res) => {
     const {userID} = req.params;
     if (!dbUsers[userID])
         res.status(404).json('Юзера не знайдено');
-    else
-        res.json(dbUsers[userID].name);
+
+    res.json(dbUsers[userID].name);
 });
 
 app.get('/cars', (req, res) => {
     res.render('cars', {dbCars});
 });
 
-app.get('/cars/:carID', (req, res) =>{
+app.get('/cars/:carID', (req, res) => {
     const {carID} = req.params;
-    if (!dbCars[carID])
+
+    if (!dbUsers[carID])
         res.status(404).json('Машину не знайдено');
-    else
-        res.json(dbCars[carID].model);
+
+    res.json(dbCars[carID].model);
 });
